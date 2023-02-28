39797c054c4372df6c07d37ff30af28781a7d4a0
Shanto Dey <126257294+shantodey@users.noreply.github.com>
2023-02-28 21:30:28 +0600

    Update castom.js

--- js/castom.js
+++ js/castom.js
@@ -1,5 +1,26 @@
 $(function(){
     
+    
+  // back to top
+  const backToTopButton = document.querySelector("#back-to-top-btn");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      backToTopButton.classList.add("show");
+    } else {
+      backToTopButton.classList.remove("show");
+    }
+  });
+  
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  
+  
+   // back to top end
 
 
   $(window).scroll(function(){
